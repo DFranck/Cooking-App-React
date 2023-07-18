ab520d390927d4c2ac5eb25222699d24e2e1b046
DFranck <franckdufournet@hotmail.fr>
2023-07-18 10:53:54 +0700

    Cooking app filter ok

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,6 @@ const Header = () => {
   return (
     <div className="header">
       <h1>Cooking App</h1>
-      <input type="text" placeholder="Tapez le nom d'un aliment (en anglais)" />
     </div>
   );
 };
--- src/components/Meals.js
+++ src/components/Meals.js
@@ -4,17 +4,41 @@ import axios from "axios";
 
 const Meals = () => {
   const [data, setData] = useState([]);
+  const [ingredientToFilter, setIngredientToFilter] = useState("");
+
+  const filterIngredient = (meal, ingredient) => {
+    for (let i = 1; i <= 20; i++) {
+      const ingredientField = `strIngredient${i}`;
+      if (
+        meal[ingredientField] &&
+        meal[ingredientField].toLowerCase().includes(ingredient.toLowerCase())
+      ) {
+        return true;
+      }
+    }
+    return false;
+  };
+
   useEffect(() => {
     axios
-      .get("https://www.themealdb.com/api/json/v1/1/search.php?s=tomato")
+      .get("https://www.themealdb.com/api/json/v1/1/search.php?s")
       .then((res) => setData(res.data.meals));
   }, []);
 
   return (
-    <div className="meals">
-      {data.map((meal) => (
-        <Card meal={meal} key={meal.idMeal} />
-      ))}
+    <div>
+      <input
+        type="text"
+        placeholder="Tapez le nom d'un aliment (en anglais)"
+        onChange={(e) => setIngredientToFilter(e.target.value)}
+      />
+      <div className="meals">
+        {data
+          .filter((meal) => filterIngredient(meal, ingredientToFilter))
+          .map((meal) => (
+            <Card meal={meal} key={meal.idMeal} />
+          ))}
+      </div>
     </div>
   );
 };
